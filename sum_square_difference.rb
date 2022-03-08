b017948b69d2d1bc073fff7d6401da19590ca6d7
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-08 13:54:19 -0800

    adds initial answer without loop

---
+++ sum_square_difference.rb
@@ -0,0 +1,14 @@
+
+
+sum_squares = (1*1) + (2*2) + (3*3) + (4*4) + (5*5) + (6*6) + (7*7) + (8*8) + (9*9) + (10*10)
+
+square_sums = (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10)
+square_sums_total = square_sums * square_sums
+
+difference = square_sums_total - sum_squares
+
+p difference
+
+# find sum of the squares of the first ten natural numbers 
+# find square of the sum of the first ten natural numbers is
+# Find the difference between the sum of the squares
