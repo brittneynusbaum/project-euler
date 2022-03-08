233ba49db7578e8ec62b6d115925c9d62249daf0
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-08 14:40:13 -0800

    adds sum square loop

--- sum_square_difference.rb
+++ sum_square_difference.rb
@@ -1,13 +1,22 @@
 
+i = 0
+sum_squares = 0
 
-sum_squares = (1*1) + (2*2) + (3*3) + (4*4) + (5*5) + (6*6) + (7*7) + (8*8) + (9*9) + (10*10)
+while i <= 10
+  sum_squares += (i * i) 
+  i += 1
+end
+p sum_squares
 
-square_sums = (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10)
-square_sums_total = square_sums * square_sums
 
-difference = square_sums_total - sum_squares
+# sum_squares = (1*1) + (2*2) + (3*3) + (4*4) + (5*5) + (6*6) + (7*7) + (8*8) + (9*9) + (10*10)
 
-p difference
+# square_sums = (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10)
+# square_sums_total = square_sums * square_sums
+
+# difference = square_sums_total - sum_squares
+
+# p difference
 
 # find sum of the squares of the first ten natural numbers 
 # find square of the sum of the first ten natural numbers is
