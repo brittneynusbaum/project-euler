ece7ba6d27cb9011eda10e79ab906ba1f5db8df9
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-08 14:46:00 -0800

    adds square sums loop

--- sum_square_difference.rb
+++ sum_square_difference.rb
@@ -1,12 +1,20 @@
 
 i = 0
 sum_squares = 0
-
 while i <= 10
   sum_squares += (i * i) 
   i += 1
 end
 p sum_squares
+
+i = 0
+square_sums = 0
+while i <= 10
+  square_sums += i
+  i += 1
+end
+
+p square_sums * square_sums
 
 
 # sum_squares = (1*1) + (2*2) + (3*3) + (4*4) + (5*5) + (6*6) + (7*7) + (8*8) + (9*9) + (10*10)
